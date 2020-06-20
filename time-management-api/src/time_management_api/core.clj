5bba9d3a15ba9c8b84325d7662185f31a3902315
Dave Martin <david.martin@fundingcircle.com>
2020-06-20 12:40:50 +0100

    Added admin routes

--- time-management-api/src/time_management_api/core.clj
+++ time-management-api/src/time_management_api/core.clj
@@ -35,6 +35,47 @@
       (bad-request {:error (str "There is already a user with email " email)}))
     (bad-request {:error (phrase.alpha/phrase-first {} :request/create-user body)})))
 
+(defn user-routes [user-id]
+  (routes
+   (context "/time-sheet" []
+     (GET "/" []
+       (let [db (datomic/user-db user-id)]
+         (ok {:time-sheet-entries (queries/get-timesheet-entries db)})))
+
+     (POST "/" {{:keys [description start duration] :as body} :body}
+       (u/with-spec
+        body :request/create-time-sheet-entry
+         (let [entry {:db/id "new"
+                      :entry/description description
+                      :entry/start (tc/to-date (tc/from-string start))
+                      :entry/duration duration
+                      :user/id user-id}
+               result @(d/transact datomic/conn (datomic/->transactions entry))]
+           (ok (-> entry
+                   (update :db/id (:tempids result))
+                   (dissoc :user/id))))))
+
+     (PUT "/:id" [id :<< as-int :as {{:keys [description start duration] :as body} :body}]
+       (u/with-spec body :request/update-time-sheet-entry
+         (let [db (datomic/user-db user-id)
+               entry {:db/id id
+                      :entry/description description
+                      :entry/start (tc/to-date (tc/from-string start))
+                      :entry/duration duration
+                      :user/id user-id}
+               existing-entity (queries/get-timesheet-entry db id)]
+           (if (some? existing-entity)
+             (do @(d/transact datomic/conn (datomic/->transactions entry existing-entity))
+                 (ok (dissoc entry :user/id)))
+             (not-found {:error (str "No time sheet entry with id " id)})))))
+
+     (DELETE "/:id" [id :<< as-int]
+       (let [db (datomic/user-db user-id)]
+         (if (some? (queries/get-timesheet-entry db id))
+           (do @(d/transact datomic/conn [[:db.fn/retractEntity id]])
+               (ok {:db/id id}))
+           (not-found {:error (str "No time sheet entry with id " id)})))))))
+
 (defroutes user-manager-only-routes
   (context "/users" []
     (GET "/" []
@@ -64,48 +105,13 @@
                 (ok updated-user))
             (not-found {:error (str "No user found with id " id)})))))))
 
+(defroutes admin-only-routes
+  (context "/users/:user-id" [user-id :<< as-int]
+    (user-routes user-id)))
+
 (defroutes authenticated-only-routes
-  (context "/" []
-    (context "/time-sheet" []
-      (GET "/" {{:keys [user-id]} :identity}
-        (let [db (datomic/user-db user-id)]
-          (ok {:time-sheet-entries (queries/get-timesheet-entries db)})))
-
-      (POST "/" {{:keys [description start duration] :as body} :body
-                 {:keys [user-id]} :identity}
-        (u/with-spec
-          body :request/create-time-sheet-entry
-          (let [entry  {:db/id "new"
-                        :entry/description description
-                        :entry/start (tc/to-date (tc/from-string start))
-                        :entry/duration duration
-                        :user/id user-id}
-                result @(d/transact datomic/conn (datomic/->transactions entry))]
-            (ok (-> entry
-                    (update :db/id (:tempids result))
-                    (dissoc :user/id))))))
-
-      (PUT "/:id" [id :<< as-int :as {{:keys [user-id]} :identity
-                                      {:keys [description start duration] :as body} :body}]
-        (u/with-spec body :request/update-time-sheet-entry
-          (let [db              (datomic/user-db user-id)
-                entry           {:db/id id
-                                 :entry/description description
-                                 :entry/start (tc/to-date (tc/from-string start))
-                                 :entry/duration duration
-                                 :user/id user-id}
-                existing-entity (queries/get-timesheet-entry db id)]
-            (if (some? existing-entity)
-              (do @(d/transact datomic/conn (datomic/->transactions entry existing-entity))
-                  (ok (dissoc entry :user/id)))
-              (not-found {:error (str "No time sheet entry with id " id)})))))
-
-      (DELETE "/:id" [id :<< as-int :as {{:keys [user-id]} :identity}]
-        (let [db (datomic/user-db user-id)]
-          (if (some? (queries/get-timesheet-entry db id))
-            (do @(d/transact datomic/conn [[:db.fn/retractEntity id]])
-                (ok {:db/id id}))
-            (not-found {:error (str "No time sheet entry with id " id)})))))
+  (context "/" {{:keys [user-id]} :identity}
+    (user-routes user-id)
 
     (context "/settings" []
       (GET "/" {{:keys [user-id]} :identity}
@@ -120,7 +126,10 @@
 
     (wrap-routes user-manager-only-routes
                  #(-> %
-                      (mw/wrap-validate-role #{:role/manager})))))
+                      (mw/wrap-validate-role #{:role/manager})))
+    (wrap-routes admin-only-routes
+                 #(-> %
+                      (mw/wrap-validate-role #{:role/admin})))))
 
 (defroutes app-routes
   (GET "/health-check" [] (ok {:healthy true}))
